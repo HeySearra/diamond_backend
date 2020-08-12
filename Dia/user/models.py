a01f909a2eb5893efaad01e705303625587fb3ba
buaacoder <wz1234@buaa.edu.cn>
2020-08-12 15:27:01 +0800

    modify message views and model add user-message views

--- Dia/user/models.py
+++ Dia/user/models.py
@@ -11,39 +11,17 @@ class User(models.Model):
     email = models.EmailField(null=True, unique=True, verbose_name='邮箱', max_length=BASIC_DATA_MAX_LEN)
     password = models.CharField(verbose_name='密码', max_length=BASIC_DATA_MAX_LEN)
     name = models.CharField(verbose_name='姓名', max_length=BASIC_DATA_MAX_LEN)
-
-    # mini fields
-    gender = models.CharField(blank=True, verbose_name='性别', max_length=MINI_DATA_MAX_LEN, choices=GENDER_CHS,
-                              default='0')
-    identity = models.CharField(blank=True, verbose_name='身份', max_length=MINI_DATA_MAX_LEN, choices=IDENTITY_CHS,
-                                default='user')
+    is_dnd = models.BooleanField(blank=True, verbose_name='消息是否免打扰', default=False)
 
     # extended fields
     intro = models.CharField(blank=True, verbose_name='简介', max_length=EXT_DATA_MAX_LEN, default='')
-    job = models.CharField(blank=True, verbose_name='职业', max_length=EXT_DATA_MAX_LEN, default='')
-    school = models.CharField(blank=True, verbose_name='学校', max_length=EXT_DATA_MAX_LEN, default='')
-    company = models.CharField(blank=True, verbose_name='公司', max_length=EXT_DATA_MAX_LEN, default='')
     session_key = models.CharField(blank=True, verbose_name='session键', max_length=EXT_DATA_MAX_LEN, default='')
 
     # other fields
     login_date = models.DateField(blank=True, verbose_name='最近登录时间', auto_now_add=True)
-    view_day = models.IntegerField(blank=True, verbose_name='最近一天浏览次数', default=0)
-    star_day = models.IntegerField(blank=True, verbose_name='最近一天收藏次数', default=0)
-    like_day = models.IntegerField(blank=True, verbose_name='最近一天被点赞次数', default=0)
-    point_day = models.IntegerField(blank=True, verbose_name='最近一天获得积分数', default=0)
-    comment_day = models.IntegerField(blank=True, verbose_name='最近一天被评论次数', default=0)
-    view_count = models.IntegerField(blank=True, verbose_name='总评论次数', default=0)
-    star_count = models.IntegerField(blank=True, verbose_name='总收藏次数', default=0)
-    like_count = models.IntegerField(blank=True, verbose_name='总被点赞次数', default=0)
-    comment_count = models.IntegerField(blank=True, verbose_name='总被评论次数', default=0)
     wrong_count = models.IntegerField(blank=True, verbose_name='最近一天密码错误次数', default=0)
     vip_date = models.DateField(blank=True, verbose_name='会员到期时间', default=date(1900, 1, 1))
     create_time = models.DateTimeField(blank=True, verbose_name='创建时间', auto_now_add=True)
-    article_id = models.IntegerField(verbose_name='置顶文章id', default=-1)
-    blocked = models.BooleanField(blank=True, verbose_name='被封禁', default=False)
-    birthday = models.DateField(blank=True, verbose_name='生日', default=date(1900, 1, 1))
-    file_size = models.IntegerField(blank=True, verbose_name='上传资源总大小', default=0)
-    point = models.IntegerField(blank=True, verbose_name='积分', default=0)
     profile_photo = models.FileField(blank=True, upload_to=DEFAULT_PROFILE_ROOT, verbose_name="头像路径", max_length=256, default='')
 
     def verify_vip(self) -> bool:
@@ -158,11 +136,11 @@ class EmailRecord(models.Model):
 
 
 class Message(models.Model):
-    uid = models.IntegerField(verbose_name='接收者id')
+    user = models.ForeignKey('user.User', related_name='id', on_delete=models.CASCADE)
     title = models.CharField(max_length=64, verbose_name='标题')
     content = models.TextField(blank=False, verbose_name='消息内容', max_length=201)
     is_read = models.BooleanField(blank=True, verbose_name='消息是否读取', default=False)
-    is_dnd = models.BooleanField(blank=True, verbose_name='消息是否免打扰', default=False)
     portrait_url = models.CharField(max_length=512, verbose_name='头像url', default='')
-    time = models.DateTimeField(default=datetime.now, verbose_name='消息产生时间')
+    dt = models.DateTimeField(default=datetime.now, verbose_name='消息产生时间')
     type = models.CharField(blank=False, verbose_name='消息类型')
+
