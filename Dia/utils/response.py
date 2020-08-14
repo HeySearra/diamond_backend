835e8c6b75025bedba28421db6e4bbaa75eead38
tky_hm_xps13h <k_code@163.com>
2020-08-15 00:23:29 +0800

    refine JSR

--- Dia/utils/response.py
+++ Dia/utils/response.py
@@ -21,23 +21,25 @@ def JSR(*keys):
             debug = meta_config.DEBUG and len(args) == 2
             if debug:
                 obj, request = args
-                func_name = meta_config.CLS_PARSE_REG.findall(str(type(obj)))[0]
+                func_name: str = meta_config.CLS_PARSE_REG.findall(str(type(obj)))[0]
+                func_name = f'{func_name[:func_name.find(".")]}, {func_name[func_name.rfind(".")+1:]}'
                 func_name = 'unknown' if len(func_name) < 2 else func_name
                 req_type = 'POST' if hasattr(request, 'body') and len(request.body) > 0 else 'GET'
-                func_name += '.' + req_type.lower()
+                # func_name += '.' + req_type.lower()
 
-                print(Fore.BLUE + f'[{req_type}] called: {func_name}')
+                # print(Fore.BLUE + f'[{req_type}] called: {func_name}')
+                para_from_frontend = ''
                 if req_type == 'POST':
                     try:
                         body_str = pformat(json.loads(request.body))
                     except:
                         body_str = '[cannot preview body]'
-                    print(Fore.BLUE + f'[{req_type}] body: {body_str}')
-                if req_type == 'GET':
-                    get_para = f'[{req_type}] session: {pformat(dict(request.session))}'
+                    para_from_frontend = f'[{req_type}, {func_name}] body: {body_str}'
+                else: # req_type == 'GET':
+                    para_from_frontend = f'[{req_type}, {func_name}] session: {pformat(dict(request.session))}'
                     if len(dict(request.GET).keys()):
-                        get_para += f', GET: {pformat(dict(request.GET))}'
-                    print(Fore.BLUE + get_para)
+                        para_from_frontend += f', GET: {pformat(dict(request.GET))}'
+                print(Fore.BLUE + para_from_frontend)
             prev_time = time.time()
             values = req_func(*args, **kw)
             time_cost = time.time() - prev_time
@@ -47,8 +49,8 @@ def JSR(*keys):
             ret_dict = dict(zip(keys, values))
             if debug:
                 c = Fore.RED if ret_dict.get('status', 0) else Fore.GREEN
-                print(c + f'[{req_type}] ret of {func_name}: {pformat(ret_dict)}')
-                print(c + f'[{req_type}] time of {func_name}: {time_cost:.2f}s')
+                print(c + f'[{req_type}, {func_name}] ret : {pformat(ret_dict)}')
+                print(c + f'[{req_type}, {func_name}] time : {time_cost:.2f}s')
             return JsonResponse(ret_dict)
 
         return wrapper
