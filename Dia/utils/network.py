d90958e29b6ce2ef0bcc52b4630ee8e90bf103c3
tky_hm_xps13h <k_code@163.com>
2020-08-15 22:17:53 +0800

    do not send email when DEBUG

--- Dia/utils/network.py
+++ Dia/utils/network.py
@@ -170,14 +170,15 @@ def send_code(acc, email_type, storage=True):
     msg['To'] = Header(to_addr)
 
     # 开启发信服务，这里使用的是加密传输
-    server = smtplib.SMTP_SSL(host='smtp.163.com')
-    server.connect(smtp_server, 465)
-    # 登录发信邮箱
-    server.login(from_addr, password)
-    # 发送邮件
-    server.sendmail(from_addr, to_addr, msg.as_string())
-    # 关闭服务器
-    server.quit()
+    if not DEBUG:
+        server = smtplib.SMTP_SSL(host='smtp.163.com')
+        server.connect(smtp_server, 465)
+        # 登录发信邮箱
+        server.login(from_addr, password)
+        # 发送邮件
+        server.sendmail(from_addr, to_addr, msg.as_string())
+        # 关闭服务器
+        server.quit()
 
     if storage:
         from user.models import EmailRecord
