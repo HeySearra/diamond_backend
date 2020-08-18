ecd9a0f0b5aea7eb76ae490a8f2cde1fba62e8d7
tky_hm_xps13h <k_code@163.com>
2020-08-18 20:19:11 +0800

    sort search

--- Dia/user/views.py
+++ Dia/user/views.py
@@ -204,7 +204,7 @@ class SearchUser(View):
         if key == 'admin_key':
             us = User.objects.all()
         else:
-            us = User.objects.all().intersection(*[User.objects.filter(Q(name__icontains=sk) | Q(acc__icontains=sk)) for sk in key.split()])
+            us = User.objects.none().union(*[User.objects.filter(Q(name__icontains=sk) | Q(acc__icontains=sk)) for sk in key.split()])
             us = sorted(
                 [_ for _ in us],
                 key=lambda x: sum([
