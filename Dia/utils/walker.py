a8828591c286cccbd6d3a74f944aac4cacc3f552
tky_hm_xps13h <k_code@163.com>
2020-08-19 00:36:16 +0800

    counts the code lines

--- Dia/utils/walker.py
+++ Dia/utils/walker.py
@@ -5,22 +5,28 @@ from colorama import Fore
 
 
 def walk_check(key):
+    lines = 0
     for path, dirs, files in os.walk('.'):
-        if '__pycache__' in path or 'migrations' in path:
+        if any(s in path for s in ['__pycache__', 'migrations', 'frontend']):
             continue
         for f_name in files:
             if not f_name.endswith('.py'):
                 continue
             f_name = os.path.join(path, f_name)
             with open(f_name, encoding='utf-8') as fp:
-                lines = [i + 1 for i, line in enumerate(fp) if key in line]
-                if len(lines) == 0:
+                contains = []
+                for i, line in enumerate(fp):
+                    lines += 1
+                    if key in line:
+                        contains.append(i+1)
+                if len(contains) == 0:
                     continue
-                print(f'`{key}` found in  {f_name:20s}:  [line {str(lines).strip("[]")}]')
+                print(f'`{key}` found in  {f_name:20s}:  [line {str(contains).strip("[]")}]')
                 # print(f''
                 #       + Fore.GREEN + f'`{key}`' + Fore.RESET + ' found in '
                 #       + Fore.CYAN + f' {f_name:20s}' + Fore.RESET + ': '
                 #       + Fore.BLUE + f'[line {str(lines).strip("[]")}]' + Fore.RESET)
+    print(f'\n[total lines of DiaDoc backend: {lines}]')
 
 
 if __name__ == '__main__':
