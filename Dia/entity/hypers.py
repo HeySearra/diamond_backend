c0b0c2a180d4b129832a6e247b25ecde595a933a
tky_hm_xps13h <k_code@163.com>
2020-08-18 19:43:48 +0800

    isprintable for check name

--- Dia/entity/hypers.py
+++ Dia/entity/hypers.py
@@ -12,5 +12,5 @@ ENT_TYPE_CHS = (
 )
 ENT_TYPE = namedtuple('___', ['fold', 'doc'])('fold', 'doc')
 
-CHECK_ENAME = lambda d: 0 <= len(d) <= ENAME_MAX_LEN
+CHECK_ENAME = lambda d: 0 <= len(d) <= ENAME_MAX_LEN and any(ch.isprintable() for ch in d)
 CHECK_COMMENT = lambda d: 0 <= len(d) <= COMMENT_MAX_LEN
