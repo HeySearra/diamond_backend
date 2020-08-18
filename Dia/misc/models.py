4231d1cc610eaaef54001c8d9f1a161fc17c212b
HeySearra <48943939+HeySearra@users.noreply.github.com>
2020-08-19 02:08:37 +0800

    auth

--- Dia/misc/models.py
+++ Dia/misc/models.py
@@ -34,85 +34,90 @@ class ReadMem(models.Model):
 
 
 class ReadAuth(models.Model):
-    user = models.ManyToManyField(to='user.User', related_name='readable_doc', through='ReadMem', through_fields=('read_auth', 'user'))
+    user = models.ManyToManyField(to='user.User', related_name='readable_doc', through='ReadMem', through_fields=('auth', 'user'))
     ent = models.ForeignKey(to='entity.Entity', related_name='read_auth', on_delete=models.CASCADE)
 
     def add_auth(self, user):
-        ra, flag = ReadMem.objects.get_or_create(user=user, read_auth=self)
+        ra, flag = ReadMem.objects.get_or_create(user=user, auth=self)
         ra.save()
         return flag
 
     def remove_auth(self, user):
         try:
-            ReadMem.objects.get(user=user, read_auth=self).delete()
+            ReadMem.objects.get(user=user, auth=self).delete()
         except:
             return False
         return True
 
     def get_user_list(self):
-        return [e.user for e in ReadMem.objects.filter(read_auth=self)]
+        return [e.user for e in ReadMem.objects.filter(auth=self)]
 
 
 class CommentAuth(models.Model):
-    user = models.ManyToManyField(to='user.User', related_name='commentable_doc', through='CommentMem', through_fields=('comment_auth', 'user'))
+    user = models.ManyToManyField(to='user.User', related_name='commentable_doc', through='CommentMem', through_fields=('auth', 'user'))
     ent = models.ForeignKey(to='entity.Entity', related_name='comment_auth', on_delete=models.CASCADE)
 
     def add_auth(self, user):
-        ra, flag = CommentMem.objects.get_or_create(user=user, comment_auth=self)
+        ra, flag = CommentMem.objects.get_or_create(user=user, auth=self)
         ra.save()
         return flag
 
     def remove_auth(self, user):
         try:
-            CommentMem.objects.get(user=user, comment_auth=self).delete()
+            CommentMem.objects.get(user=user, auth=self).delete()
         except:
             return False
         return True
 
     def get_user_list(self):
-        return [e.user for e in CommentMem.objects.filter(comment_auth=self)]
+        return [e.user for e in CommentMem.objects.filter(auth=self)]
 
 
 class WriteAuth(models.Model):
-    user = models.ManyToManyField(to='user.User', related_name='writeable_doc', through='WriteMem', through_fields=('write_auth', 'user'))
+    user = models.ManyToManyField(to='user.User', related_name='writeable_doc', through='WriteMem', through_fields=('auth', 'user'))
     ent = models.ForeignKey(to='entity.Entity', related_name='write_auth', on_delete=models.CASCADE)
 
     def add_auth(self, user):
-        ra, flag = WriteMem.objects.get_or_create(user=user, write_auth=self)
+        ra, flag = WriteMem.objects.get_or_create(user=user, auth=self)
         ra.save()
         return flag
 
     def remove_auth(self, user):
         try:
-            WriteMem.objects.get(user=user, write_auth=self).delete()
+            WriteMem.objects.get(user=user, auth=self).delete()
         except:
             return False
         return True
 
     def get_user_list(self):
-        return [e.user for e in WriteMem.objects.filter(write_auth=self)]
+        return [e.user for e in WriteMem.objects.filter(auth=self)]
+
+
+class ShareMem(models.Model):
+    user = models.ForeignKey(to='user.User', on_delete=models.CASCADE)
+    auth = models.ForeignKey(to='ShareAuth', on_delete=models.CASCADE)
+    dt = models.DateTimeField(auto_now_add=True)    # 预留给工作台和文件系统的排序
+
+    @property
+    def dt_str(self):
+        return self.dt.strftime(TIME_FMT)
 
 
 class ShareAuth(models.Model):
-
-    user = models.ManyToManyField(to='user.User', related_name='shared_doc', null=True, blank=True)
-    owner = models.ManyToManyField(to='user.User', related_name='shareable_doc', null=True, blank=True)
+    user = models.ManyToManyField(to='user.User', related_name='shared_doc', blank=True, through='ShareMem', through_fields=('auth', 'user'))
+    owner = models.ManyToManyField(to='user.User', related_name='shareable_doc', blank=True)
     ent = models.ForeignKey(to='entity.Entity', related_name='share_auth', on_delete=models.CASCADE)
     auth = models.CharField(max_length=64, verbose_name="分享的权限", choices=AUTH_TYPE_CHS, default='no_share')
     key = models.CharField(max_length=64, verbose_name='可写权限密钥', default='')
 
     def add_auth(self, user):
-        try:
-            self.user.add(user)
-            self.save()
-        except:
-            return False
-        return True
+        ra, flag = ShareMem.objects.get_or_create(user=user, auth=self)
+        ra.save()
+        return flag
 
     def remove_auth(self, user):
         try:
-            self.user.remove(user)
-            self.save()
+            ShareMem.objects.get(user=user, auth=self).delete()
         except:
             return False
         return True
